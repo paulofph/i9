f9c38ab5b1bd866c4c133dc0d5d1feebc5e1bbe3
Paulo Henriques <paulo.henriques1993@gmail.com>
2018-06-15 23:38:24 +0100

    Add icons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import Button from '@material-ui/core/Button';
-
+import SideNav, { Nav, NavIcon, NavText } from 'react-sidenav';
 import logo from './logo.svg';
 import './App.css';
 
