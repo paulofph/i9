868c5339d5edc4c65db7c3b638c163c723e3e883
Paulo Henriques <paulo.henriques1993@gmail.com>
2018-06-23 17:34:03 +0100

    pflex working

--- src/containers/Root.js
+++ src/containers/Root.js
@@ -3,13 +3,27 @@ import PropTypes from 'prop-types'
 import { Provider } from 'react-redux'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import App from './../App'
+import './../index.scss'
 
 const Root = ({ store }) => (
-  <Provider store={store}>
-    <Router>
-      <Route path="/:filter?" component={App} />
-    </Router>
-  </Provider>
+  <React.Fragment>
+    <div className="pflex-row">
+      <div className="pflex-column pflex-20p">
+        test1
+      </div>
+
+      <div className="pflex-column pflex-20p">
+        test2
+      </div>
+    </div>
+    <div>
+    <Provider store={store}>
+      <Router>
+        <Route path="/:filter?" component={App} />
+      </Router>
+    </Provider>
+    </div>
+  </React.Fragment>
 )
 
 Root.propTypes = {
